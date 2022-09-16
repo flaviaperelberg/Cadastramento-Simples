40cd6a6963ce6dc0511b4cdf3a25d5d1ef04be7e
Flávia <113394221+flaviaperelberg@users.noreply.github.com>
2022-09-16 19:33:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Implemente um programa capaz de simular o cadastramento de um funcionário de uma empresa. Para isso, deve perdir os seguintes dados: nome, endereço, idade, telefone, cargo, salário. Ao final do cadastro, o programa deve imprimir: Cadastro realizado com sucesso. 
+
+print ('Bem-vindos(a) ao cadastramento de funcionários, por favor coloque seus dados quando for solicitado:')
+
+nome = input('Digite seu nome:\n ')
+
+endereco = input('Digite seu endereço completo: \n ')
+
+idade = input('Digite sua idade: \n ')
+
+telefone = input('Digite seu telefone: \n ')
+
+cargo = input('Digite seu cargo: \n')
+
+salário = input('Digite seu salário: \n ')
+
+print ('Cadastro realizado com sucesso')
